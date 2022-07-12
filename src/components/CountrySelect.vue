f7f7dcb6d9a584da3a71ac26243fca8e7ccbfce9
Luis <40062408+lsanchez8@users.noreply.github.com>
2022-07-11 20:03:36 -0400

    feat: add covid tracker app

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,35 @@
+<template>
+  <select
+    @change="onChange()"
+    v-model="selected"
+    class="form-select block w-full border p-3 rounded mt-10 mb-3"
+  >
+    <option value="0">Select Country</option>
+    <option
+      v-for="country in countries"
+      :value="country.ID"
+      v-bind:key="country.ID"
+    >
+      {{ country.Country }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: ["countries"],
+  data() {
+    return {
+      selected: 0,
+    };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selected);
+      console.log(country);
+      this.$emit("get-country", country);
+    },
+  },
+};
+</script>
